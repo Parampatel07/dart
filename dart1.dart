b8db9ff51a333ef303ff3ee076f45320a23907bb
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-10-12 09:12:41 +0530

    variables

---
+++ dart1.dart
@@ -0,0 +1,22 @@
+void main()
+{
+    int age = 18;
+    double weight = 50.505;
+    bool gender = true;
+    String name = "The Easylearn academy ";
+
+    print(age);
+    print(weight);
+    print(gender);
+    print(name);
+
+    print("My age is $age ");
+    print("My weight is $weight");
+    print("My Gender is $gender ");
+    print("My name is $name ");
+
+    print("\n\nMy age is "+age.toString());
+    print("My weight is " +weight.toString());
+    print("My Gender is " +gender.toString());
+    print("My name is "+name );
+}
---
+++ dynamic.dart
@@ -0,0 +1,13 @@
+// example of dynamic datatype 
+void main()
+{
+    dynamic temp;
+    temp="Param patel ";
+    print(temp);
+    temp=85.85;
+    print(temp);
+    temp=8;
+    print(temp);
+    temp=false;
+    print(temp);
+}
